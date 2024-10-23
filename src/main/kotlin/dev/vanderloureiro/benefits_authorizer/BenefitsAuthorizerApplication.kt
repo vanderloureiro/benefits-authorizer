8e88066cdf0b4875c1b4c4eb7735e79c94264c1e
Vanderlei Loureiro <vanderloureiroleite@gmail.com>
2024-10-22 21:36:49 -0300

    inicio do projeto

---
+++ src/main/kotlin/dev/vanderloureiro/benefits_authorizer/BenefitsAuthorizerApplication.kt
@@ -0,0 +1,11 @@
+package dev.vanderloureiro.benefits_authorizer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BenefitsAuthorizerApplication
+
+fun main(args: Array<String>) {
+	runApplication<BenefitsAuthorizerApplication>(*args)
+}
---
+++ src/test/kotlin/dev/vanderloureiro/benefits_authorizer/BenefitsAuthorizerApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.vanderloureiro.benefits_authorizer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BenefitsAuthorizerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
